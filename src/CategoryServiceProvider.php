ec95a9961d9c8a152937757bcd49df9485f6a716
Marco Schauer <marco.schauer@mdm.de>
2019-11-06 12:34:53 +0100

    Init empty module

---
+++ src/CategoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author     Marco Schauer <marco.schauer@darkdevelopers.de.de>
+ * @copyright  2019 Marco Schauer
+ */
+
+namespace Vof\Category;
+
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CategoryServiceProvider
+ * @package Vof\Category
+ */
+class CategoryServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        /*$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'vof.admin.usermanagment');
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'vof.admin.usermanagment');*/
+    }
+
+    public function register()
+    {
+
+    }
+}
